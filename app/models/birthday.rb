1926d828eec89a53d62db1cfb2dbecb2c90580e6
Attsun1031 <atsumitatsuya@progress.local>
2013-02-24 15:16:08 +0900

    refactor post method

--- app/models/birthday.rb
+++ app/models/birthday.rb
@@ -7,31 +7,45 @@ class Birthday < ActiveRecord::Base
 
   MESSAGE_TEMPLATE = '今日は%s%sの誕生日！%s'
 
+  # 誕生日メッセージをポストする
+  def post_birthday
+    message = build_birthday_message
+    Twitter.request_for_birthday_bot(:update_status, message)
+  end
+
+  # birthday で検索を行う
+  # ==== Args
+  # target_date :: 対象の日付。
+  # ignore_year :: 年を無視して検索を行うか。デフォルト true
+  def self.find_by_birthday target_date, ignore_year = true
+    unless ignore_year
+      return super target_date
+    end
+    where_clause = 'birthday LIKE ?', "%" + target_date.strftime("%m%d")
+    return Birthday.where where_clause
+  end
+
   # 対象日が誕生日のデータを Twitter にポストする
-  def self.post_birthday target_date
-    birthdays = Birthday.where('birthday LIKE ?', "%" + target_date.strftime("%m%d"))
-    successes = []
-    errors = []
-
-    birthdays.each do |employee|
-      # take interval between tweet
-      sleep 5
-
-      name = employee.name_ja.present? ? employee.name_ja : employee.name_en
-      tweet = MESSAGE_TEMPLATE % [
-        employee.introduction,
-        name,
-        employee.comment
-      ]
+  def self.post_birthdays target_date
+    birthdays = find_by_birthday target_date
+    birthdays.each do |birthday|
+      take_interval_between_post
       begin
-        Twitter.request_for_birthday_bot(:update_status, tweet)
-        successes.append employee
+        birthday.post_birthday
       rescue Twitter::Exceptions::InvalidStatusException => exception
         logger.error "Failed to post birthday. name: %s, reason: %s" % [name, exception.message]
-        errors.append employee
       end
     end
+  end
 
-    return successes, errors
+  private
+  def build_birthday_message
+    name = name_ja.present? ? name_ja : name_en
+    tweet = MESSAGE_TEMPLATE % [introduction, name, comment]
+    return tweet
+  end
+
+  def self.take_interval_between_post
+    sleep 5
   end
 end
--- spec/models/birthday_spec.rb
+++ spec/models/birthday_spec.rb
@@ -5,7 +5,7 @@ require 'date'
 require 'twitter'
 
 describe Birthday do
-  describe "post_birthday" do
+  describe "post_birthdays" do
     fixtures :birthdays
 
     before do
@@ -17,25 +17,19 @@ describe Birthday do
       Birthday.should_receive(:sleep).with(5).twice
       Twitter.should_receive(:request_for_birthday_bot).with(:update_status, "今日はDiana Rossの誕生日！").ordered
       Twitter.should_receive(:request_for_birthday_bot).with(:update_status, "今日は世界を代表するジャズ・ピアニストである上原ひろみの誕生日！Brainは名盤でしょう。").ordered
-      successes, errors = Birthday.post_birthday Date.new(2013, 3, 26)
-      successes[0].name_en.should == "Diana Ross"
-      successes[1].name_ja.should == "上原ひろみ"
+      Birthday.post_birthdays Date.new(2013, 3, 26)
     end
 
     it "should not post" do
       Twitter.should_not_receive(:request_for_birthday_bot)
-      successes, errors = Birthday.post_birthday Date.new(2013, 3, 27)
-      successes.should be_empty
-      errors.should be_empty
+      Birthday.post_birthdays Date.new(2013, 3, 27)
     end
 
     context "over 140 chars" do
       it "should raise error" do
         Twitter.should_receive(:request_for_birthday_bot).with(:update_status, "今日はMax Roachの誕生日！").ordered
         Twitter.should_receive(:request_for_birthday_bot).with(:update_status, "今日はドイツの生んだ早熟の天才、マイケル・シェンカーの誕生日！").ordered.and_raise(Twitter::Exceptions::InvalidStatusException)
-        successes, errors = Birthday.post_birthday Date.new(2013, 1, 10)
-        successes[0].name_en.should == "Max Roach"
-        errors[0].name_ja.should == "マイケル・シェンカー"
+        Birthday.post_birthdays Date.new(2013, 1, 10)
       end
     end
   end
